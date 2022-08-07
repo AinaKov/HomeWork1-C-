d411055a8a8a65a78c0f6c022ae053ac4e40f219
AlinaKov <alkovkova@gmail.com>
2022-08-08 02:52:27 +0500

    Added folder Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿while (true)
+    {
+    Console.WriteLine("Хотите узнать какое число больше? Если хотите выйти из программы - напишите no");
+    string answer = Console.ReadLine();
+    if(answer == "no")
+    {
+        break;
+    }
+    Console.WriteLine("Введите первое число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    if(number1 > number2)
+    {
+       Console.WriteLine("Наибольшее число первое");
+       Console.WriteLine(number1);
+    }
+    else
+    {
+       Console.WriteLine("Наибольшее число второе");
+       Console.WriteLine(number2);
+    }
+    }
