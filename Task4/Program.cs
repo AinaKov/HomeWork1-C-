4d23f9f330b6fbc02382c18c73d751af457621f8
AlinaKov <alkovkova@gmail.com>
2022-08-08 02:53:06 +0500

    Added folder Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿while (true)
+    {
+    Console.WriteLine("Хотите узнать какое из трех чисел больше? Если хотите выйти из программы - напишите no");
+    string answer = Console.ReadLine();
+    if(answer == "no")
+    {
+        break;
+    }
+    Console.WriteLine("Введите первое число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите третье число");
+    int number3 = Convert.ToInt32(Console.ReadLine());
+
+    if(number1 > number2 & number1 > number3)
+       { 
+        Console.WriteLine("Наибольшее число первое");
+        Console.WriteLine(number1);
+       }
+    else if(number2 > number1 & number2 > number3)
+         { 
+         Console.WriteLine("Наибольшее число второе");
+         Console.WriteLine(number2);
+         } 
+    else if(number3 > number1 & number3 > number2)
+         { 
+         Console.WriteLine("Наибольшее число третье");
+         Console.WriteLine(number3);
+         } 
+    }
