1e9f9b9f5aa7aeb7ef20b79c7d4c3b2a22c12a58
AlinaKov <alkovkova@gmail.com>
2022-08-08 02:53:33 +0500

    Added folder Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿while (true)
+{
+    Console.WriteLine("Хотите узнать все четные числа до числа N? Если хотите выйти из программы - напишите end");
+    string answer = Console.ReadLine();
+    if(answer == "end")
+     {    
+     break;
+     }
+    Console.WriteLine("Введите число N");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int number1 = 2;
+         if(number >= 2)
+         {
+            while (number1 <= number)
+             {
+                Console.Write(number1 );
+                Console.Write(", ");
+                number1 = number1 + 2;
+             }
+         }
+         else  
+         {
+          Console.WriteLine("Таких чисел не сущестует");
+         }
+     Console.WriteLine(" ");
+}
