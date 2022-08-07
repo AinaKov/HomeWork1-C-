4360859dfd7a093d4967547542a228c045b274f9
AlinaKov <alkovkova@gmail.com>
2022-08-08 02:53:17 +0500

    Added folder Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿while (true)
+{
+   Console.WriteLine("Давайте узнаем - четное ли число? напишите end, если хотите выйти из программы");
+   string answer = Console.ReadLine();
+      if(answer == "end")
+       {    
+       break;
+       }
+   Console.WriteLine("Введите число");
+   int number1 = Convert.ToInt32(Console.ReadLine());
+    if(number1 % 2 == 0)
+    {
+    Console.WriteLine("Число четное"); 
+    }
+    else
+    {
+    Console.WriteLine("Число нечетное");
+    }
+}
